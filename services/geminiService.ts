4302b81795f7df3a5c591a751aa1029201c1b507
shpolice <66867141+shpolice@users.noreply.github.com>
2026-03-11 16:54:58 +0700

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -2,7 +2,7 @@ export async function askGemini(prompt: string) {
   const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 
   const res = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
     {
       method: "POST",
       headers: {
