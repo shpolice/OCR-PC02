fefbf1d149f178bc3da5b19716417fed7fe1db26
shpolice <66867141+shpolice@users.noreply.github.com>
2026-03-11 17:59:32 +0700

    fix gemini service export

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,7 +1,8 @@
 export async function extractConsolidatedInfo(text: string) {
+
   const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 
-  const res = await fetch(
+  const response = await fetch(
     `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
     {
       method: "POST",
@@ -18,12 +19,12 @@ export async function extractConsolidatedInfo(text: string) {
     }
   );
 
-  const data = await res.json();
+  const data = await response.json();
 
-if (!data.candidates) {
-  console.error(data);
-  return "Gemini API error";
-}
+  if (!data.candidates) {
+    console.error(data);
+    return "Gemini API error";
+  }
 
-return data.candidates[0].content.parts[0].text;
+  return data.candidates[0].content.parts[0].text;
 }
