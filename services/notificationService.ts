86d7be71303ead8d59f422c88274264845bc5cf2
shpolice <66867141+shpolice@users.noreply.github.com>
2026-03-11 14:54:03 +0700

    Create notificationService.ts

---
+++ services/notificationService.ts
@@ -0,0 +1,20 @@
+export async function sendDataToWebhook(data: any) {
+  const WEBHOOK_URL = import.meta.env.VITE_WEBHOOK_URL;
+
+  if (!WEBHOOK_URL) {
+    console.log("Webhook chưa cấu hình");
+    return;
+  }
+
+  try {
+    await fetch(WEBHOOK_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+  } catch (err) {
+    console.error("Webhook error:", err);
+  }
+}
